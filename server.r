3bc0184d141d3584dc96b4083095231a69d61f4f
darcy <creedd23@mail.wlu.edu>
2022-11-16 14:48:10 -0500

    commit

--- server.r
+++ server.r
@@ -17,11 +17,9 @@ function(input, output, session) {
     }, options = list(pageLength = 1))
  
      #Compare
-    output$compareoutput1 <- renderPrint(input$compareinput1)
-    output$compareoutput2 <- renderPrint(input$compareinput2)
-    output$compareoutput3 <- renderPrint(input$compareinput3)
-    output$compareoutput4 <- renderPrint(input$compareinput4)
-    output$compareoutput5 <- renderPrint(input$compareinput5)
-  
-  
+    output$plot <- renderPlot({
+      input$goButton
+      plot(compareinput1:compareinput5, ), res = 96)
+    }
 }
+)
